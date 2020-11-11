274e27cdde7849050180b9a795837a7a9b750da4
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-11-11 01:26:49 -0800

    buttons have been added with the corresponding functionality

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,31 +1,51 @@
 import React from "react";
 import {render} from "react-dom";
+import grid from "../assets/grid.png";
 
 class Drawer extends React.Component{
 	
 	constructor(props){
 		super(props);
-		this.state = {x:NaN,y:NaN,
-			      width:400,height:400};
+		this.state = {x:NaN,y:NaN,hoverMode:false};
+		this.width = this.height = 400;
 	}
 
 	onMouseMove(e){
-	 	this.setState({ x: (e.nativeEvent.offsetX/this.state.width).toFixed(3),
-				y: (1-e.nativeEvent.offsetY/this.state.height).toFixed(3)});
+	 	this.setState({ x: (e.nativeEvent.offsetX/this.width).toFixed(3),
+				y: (1-e.nativeEvent.offsetY/this.height).toFixed(3)},
+			()=>{if(this.state.hoverMode) this.addCoord()}
+		);	
+	}
+
+	toggleHoverMode(){
+		this.setState({hoverMode:!this.state.hoverMode});
 	}
 	
-	onClick(){
-		const coords = {x:this.state.x,
-				y:this.state.y};
-		this.props.setCoords(coords); 
+	addCoord(){
+		const coord = {x:this.state.x,
+		               y:this.state.y};
+		this.props.addCoord(coord); 
 	}
+
 		
 
 	render(){
 		return(
 			<div>
-				<div onMouseMove={(e)=>this.onMouseMove(e)}  style={{width:this.state.width,height:this.state.height,backgroundColor:"rgba(100,100,100,0.2)"}}
-			 onClick={this.onClick.bind(this)}/>
+				<div className="row mb-4">
+					<div className="col-6">
+						<p>Hover Mode: [{this.state.hoverMode?"ON":"OFF"}]</p>
+						<button type="button" className="btn btn-success" onClick={this.toggleHoverMode.bind(this)}>toggle</button>
+					</div>
+					<div className="col-6">
+						<p>Reset Button:</p>
+						<button type="button" className="btn btn-danger" onClick={this.props.resetCoords}>reset</button>
+					</div>
+				</div>
+
+				<div onMouseMove={(e)=>this.onMouseMove(e)}  
+			style={{width:this.width,height:this.height,backgroundColor:"rgba(100,100,100,0.1)",backgroundImage:`url(${grid})`}}
+			onClick={this.addCoord.bind(this)}/>
 				<div className="row mt-2">
 					<div className="col-6">
 						<p>X: {this.state.x}</p>
@@ -33,11 +53,12 @@ class Drawer extends React.Component{
 					<div className="col-6">
 						<p>Y: {this.state.y}</p>
 					</div>
-				</div>
+				</div>	
+				
 			</div>
 		);
 	}
-
 }
+
 
 export default Drawer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -13,13 +13,17 @@ class Main extends React.Component{
 		};
 	}
 
-	setCoords(coords){
-		const new_x_array = this.state.x_array.concat(coords.x);
-		const new_y_array = this.state.y_array.concat(coords.y);
+	addCoord(coord){
+		const new_x_array = this.state.x_array.concat(coord.x);
+		const new_y_array = this.state.y_array.concat(coord.y);
 		this.setState({
 			x_array:new_x_array,
 			y_array:new_y_array
 		});
+	}
+
+	resetCoords(){
+		this.setState({x_array:new Array(), y_array:new Array()});
 	}
 		
 	render(){
@@ -27,7 +31,7 @@ class Main extends React.Component{
 			<div className="container-fluid pt-4">
 				<div className="row justify-content-center">
 					<div className="col-12 col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center">
-						<Drawer setCoords={this.setCoords.bind(this)}/>
+						<Drawer addCoord={this.addCoord.bind(this)} resetCoords={this.resetCoords.bind(this)}/>
 					</div>
 					<div className="col-12 col-sm-12 col-md-12 col-lg-8">
 						<Graph x={this.state.x_array} y={this.state.y_array} deg={1} width={800} color={"rgb(255,0,0)"}/>
