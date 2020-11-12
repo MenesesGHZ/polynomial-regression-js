dd33d915cbe3fb655b525d95f2039a622c62ec81
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-11-12 13:33:40 -0800

    Adapted to visualize the polynomial regression fit at real time

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {render} from "react-dom";
+import PolynomialRegression from "../polynomial_regression/PolynomialRegression";
 const Chart = require('chart.js');
 
 class Graph extends React.Component{
 	constructor(props){
-		super(props);	
+		super(props);
+		this.model = new PolynomialRegression();
 		this.chartRef = React.createRef();
 	}
 
@@ -13,33 +15,48 @@ class Graph extends React.Component{
 	}
 
 	componentDidUpdate() {
-		this.plot_update();	
+		if(this.props.x.length!=0 && this.props.x.length!=0) this.model.train(this.props.x,this.props.y,this.props.deg);
+		this.plot_update();
 	}
-
+	
 	plot_update(){
-		let data = new Array();
+		let data = new Array(),
+			regression_points = new Array();
 		for(let i=0;i<this.props.x.length;i++){
 			data.push(
 				{x:this.props.x[i],y:this.props.y[i]}
 			);
 		}
+		for(let i=0;i<1;i+=0.05){
+			regression_points.push({x:i,y:this.model.predict(i)});
+		}
 		this.chart.data.datasets[0].data = data;
-		this.chart.data.datasets[0].pointBackgroundColor.push(this.props.color);
+		this.chart.data.datasets[0].label = `Polynomial Degree: ${String(this.props.deg)}`;
+		this.chart.data.datasets[0].pointBackgroundColor.push("rgba(0,0,0,1)");
+		this.chart.data.datasets[1] =  {
+			type:'line',
+			label: this.model.expression,
+			function: function(x){return this.model.predict(x)},
+			data: regression_points,
+			borderColor: "rgba(0,0,255,1)",
+			fill: false 
+		}
 		this.chart.update();
 	}
+	
 
 	plot(){
 		this.chart = new Chart(this.chartRef.current, {
-			type:"scatter",
+			type:'scatter',
 			data:{
 				datasets:[{
-					label:"Polynomial Degree: "+String(this.props.deg),
+					type:"line",
+					label:`Polynomial Degree: ${String(this.props.deg)}`,
 					data:new Array(),
+					borderColor: this.props.color,
+					fill: false, 
 					pointBackgroundColor:new Array()
 				}]
-			},
-			options:{
-				responsive:true
 			}
 		});
 	}
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,16 @@ import React from "react";
 import {render} from "react-dom";
 import Graph from "./Graph";
 import Drawer from "./Drawer";
-
+import RegressionSetup from "./RegressionSetup";
 
 class Main extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			x_array: new Array(),
-			y_array: new Array()
+			y_array: new Array(),
+			order_graph_1:1,
+			order_graph_2:2,
 		};
 	}
 
@@ -21,21 +23,30 @@ class Main extends React.Component{
 			y_array:new_y_array
 		});
 	}
-
+	
 	resetCoords(){
 		this.setState({x_array:new Array(), y_array:new Array()});
 	}
-		
+
+	setOrders(orders){
+		this.setState({
+			order_graph_1:orders[0],
+			order_graph_2:orders[1]
+		});
+	}
+
+
 	render(){
 		return(
 			<div className="container-fluid pt-4">
 				<div className="row justify-content-center">
-					<div className="col-12 col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center">
+					<div className="col-12 col-sm-12 col-md-12 col-lg-4 ">
 						<Drawer addCoord={this.addCoord.bind(this)} resetCoords={this.resetCoords.bind(this)}/>
+						<RegressionSetup order_graph_1={this.state.order_graph_1} order_graph_2={this.state.order_graph_2} setOrders={this.setOrders.bind(this)} /> 	
 					</div>
 					<div className="col-12 col-sm-12 col-md-12 col-lg-8">
-						<Graph x={this.state.x_array} y={this.state.y_array} deg={1} width={800} color={"rgb(255,0,0)"}/>
-						<Graph x={this.state.x_array} y={this.state.y_array} deg={2} width={800} color={"rgb(255,0,0)"}/>
+						<Graph x={this.state.x_array} y={this.state.y_array} deg={this.state.order_graph_1} width={800} color={"rgb(255,0,0)"} />
+						<Graph x={this.state.x_array} y={this.state.y_array} deg={this.state.order_graph_2} width={800} color={"rgb(0,255,0)"} />
 					</div>
 				</div>
 			</div>
