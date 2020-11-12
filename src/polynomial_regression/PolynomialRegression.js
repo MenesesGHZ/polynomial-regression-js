92c673cc9ef2f3284431966ff371bd3788212452
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-11-12 00:19:49 -0800

    PolynomialRegression model completed

--- src/polynomial_regression/PolynomialRegression.js
+++ src/polynomial_regression/PolynomialRegression.js
@@ -10,57 +10,51 @@ class PolynomialRegression{
 	 */
 
 	constructor(){
-		this.model_expression = null;
+		this.expression = "f(x) = undefined";
+		this.coefficients = new Array(); 
 		this.degree = NaN;
+		this.f = undefined;
 	}	
 
 	train(X,y,degree=1){
-		//full batch fashion ;)
-		const n = math.size(X)[0],
-			this.degree = degree;
-//		//responses
-//		const y_predicted = this.predict(x);
-//		//residuals
-//		const r = math.subtract(y,y_predicted);
-//		//sum of the square of the residuals
-//		const sqr = math.sum(math.pow(r));
-		//computing derivates of the `polynomial model` with respect of `a_i`, and sums
-		let derivates_sqr = new Array(),
-			x_pow_deg = new Array(),
-			s_xiy = new Array(),
-			der_sqr_ai = null;
-		for(let i=0;i<=this.degree;i++){
-			// [ X^0, X^1, ... , X^i ]
-			x_pow_deg.push(math.pow(X,i));
-			
-		//	//computing derivates
-		//	der_sqr_ai = math.multiply(-2,math.multiply(x_pow_deg[i],r));
-		//	derivates_sqr.push(der_sqr_ai);
+		const n = X.length;
+		let x_pow_deg = new Array(),
+			s_xiy = new Array();
+		this.degree = degree;
 
-			//computing sums
+		// computing sum of Xs to the power of `i` -> [ X^0, X^1, ... , X^i ], and computing sums
+		for(let i=0;i<=this.degree;i++){
+			x_pow_deg.push(X.map(el=>el**i));
 			s_xiy.push(math.multiply(x_pow_deg[i],y));
 		}
-		//computing remaing sums for solving the `m+1` linear equations, 
-		//where `m` is equal to the number of coeficients; m=this.degree
-		for(let i=this.degree+1; i<=2*this.degree;i++){
-			x_pow_deg.push(math.pow(X,i));
-		}
-		let U = math.matrix(math.zeros([this.degree+1,this.degree+1])).data
-		//fixing equation
-		for(let i=0;i<this.degree;i++){ 
-			for(let j=0;j<this.degree;j++){
-				U[i][j] = x_pow_deg[j+i];
-			}
-		}
-		const coef = math.usolve(U,s_xiy);
-		console.log(coef)
+
+		//computing remaing sums for solving the `m+1` linear equations, where `m` is equal to the number of coeficients; m=this.degree
+		for(let i=this.degree+1; i<=2*this.degree;i++) 
+			x_pow_deg.push(X.map(el=>el**i)); 
+		
+		//fixing equation; Ax = b -> A*s_xiy = coef	
+		let A = math.zeros(this.degree+1,this.degree+1)._data
+		for(let i=0;i<=this.degree;i++)
+			for(let j=0;j<=this.degree;j++)
+				A[i][j] = math.sum(x_pow_deg[j+i]);
+		A[0][0] = n;
+
+		//solve linear equation Ax = b |  modeling f(x) in JS  |  fomating mathematical expression of the model
+		this.coefficients = math.usolve(A,s_xiy);
+		this.f = (x) =>{
+			return math.sum(this.coefficients.map((coef,i)=>coef*(x**i)))
+		};
+		this.expression = "f(x) = ".concat(
+			this.coefficients.map((el,i)=> i===0? `${el}`:` + ${el}x^${i}` ).join("")
+		);
+		
 	}
 
-	predict(x){
-		/**
-		 * x: A column vector of scalars, or a scalar.
-		 * return: A column vector of responses, or a scalar.   
-		 */
+	predict(X){
+		// x: row vector.
+		// return: row vector.   
+		const response = X.map((x)=>this.f(x));
+		return response;
 	}
 }
 
