13ab434c388f611d0e9e4f9064baeec8af8c4002
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-11-12 21:51:07 -0800

    fixed bug: whenever the algorithm tried to solve a system of linear equations where the determinant of a matrix is equal to 0, it fails solving with the LU descomposition

--- src/polynomial_regression/PolynomialRegression.js
+++ src/polynomial_regression/PolynomialRegression.js
@@ -25,37 +25,42 @@ class PolynomialRegression{
 	}	
 
 	train(X,y,degree=1){
-		const n = X.length;
-		let x_pow_deg = new Array(),
-			s_xiy = new Array();
-		this.degree = degree;
+		try {
+			const n = X.length;
+			let x_pow_deg = new Array(),
+				s_xiy = new Array();
+			this.degree = degree;
 
-		// computing sum of Xs to the power of `i` -> [ X^0, X^1, ... , X^i ], and computing sums
-		for(let i=0;i<=this.degree;i++){
-			x_pow_deg.push(X.map(x=>x**i));
-			s_xiy.push(math.multiply(x_pow_deg[i],y));
+			// computing sum of Xs to the power of `i` -> [ X^0, X^1, ... , X^i ], and computing sums
+			for(let i=0;i<=this.degree;i++){
+				x_pow_deg.push(X.map(x=>x**i));
+				s_xiy.push(math.multiply(x_pow_deg[i],y));
+			}
+
+			//computing remaining sums for solving the `m+1` linear equations, where `m` is equal to the number of coeficients; m=this.degree
+			for(let i=this.degree+1; i<=2*this.degree;i++) 
+				x_pow_deg.push(X.map(el=>el**i)); 
+
+			//fixing equation; Ax = b -> A*s_xiy = coef	
+			let A = math.zeros(this.degree+1,this.degree+1)._data
+			for(let i=0;i<=this.degree;i++)
+				for(let j=0;j<=this.degree;j++)
+					A[i][j] = math.sum(x_pow_deg[j+i]);
+			A[0][0] = n;
+
+			//solve linear equation Ax = b |  modeling f(x) in JS  |  fomating mathematical expression of the model
+			this.coefficients = math.lusolve(math.lup(A),s_xiy)._data;
+			this.f = (x) =>{
+				return math.sum(this.coefficients.map((coef,i)=>coef*(x**i)))
+			};
+			this.expression = "f(x) = ".concat(
+				this.coefficients.map((el,i)=> i===0? `${el}`:` + (${el}x^${i})` ).join("")
+			);
+
+		} catch (error) {
+			// Error: Linear system cannot be solved since matrix is singular. |Matrix|=Det(Matrix)=0
+			console.error(error);
 		}
-
-		//computing remaining sums for solving the `m+1` linear equations, where `m` is equal to the number of coeficients; m=this.degree
-		for(let i=this.degree+1; i<=2*this.degree;i++) 
-			x_pow_deg.push(X.map(el=>el**i)); 
-		
-		//fixing equation; Ax = b -> A*s_xiy = coef	
-		let A = math.zeros(this.degree+1,this.degree+1)._data
-		for(let i=0;i<=this.degree;i++)
-			for(let j=0;j<=this.degree;j++)
-				A[i][j] = math.sum(x_pow_deg[j+i]);
-		A[0][0] = n;
-
-		//solve linear equation Ax = b |  modeling f(x) in JS  |  fomating mathematical expression of the model
-		this.coefficients = math.lusolve(math.lup(A),s_xiy)._data;
-		this.f = (x) =>{
-			return math.sum(this.coefficients.map((coef,i)=>coef*(x**i)))
-		};
-		this.expression = "f(x) = ".concat(
-			this.coefficients.map((el,i)=> i===0? `${el}`:` + (${el}x^${i})` ).join("")
-		);
-		
 	}
 
 	predict(X){
